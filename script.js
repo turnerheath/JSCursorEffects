3b4f46fde5ab637cc9c0f0e59822de2186729336
turnerheath <123667090+turnerheath@users.noreply.github.com>
2024-02-03 14:23:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+class Root {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.speedX = Math.random() * 4 - 2;
+        this.speedY = Math.random() * 4 - 2;
+        this.maxSize = Math.random() * 1;
+        this.size = Math.random() * 3 + 7;
+        this.vs = Math.random() * 0.2 + 0.05;
+        this.angleX = Math.random() * 6.2;
+        this.vax = Math.random() * 0.6 - 0.3;
+        this.angleY = Math.random() * 6.2;
+        this.vay = Math.random() * 0.6 - 0.3;
+        this.lightness = 10;
+        this.brushcolor = brushcolor;
+    }
+    update() {
+        this.x += this.speedX + Math.sin(this.angleX);
+        this.y += this.speedY + Math.sin(this.angleY);
+        this.size -= this.vs;
+        this.angleX += this.vax;
+        this.angleY += this.vay;
+        if (this.lightness < 70) this.lightness += 0.5;
+        if (this.size > this.maxSize) {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.fillStyle = 'hsl(' + brushcolor.innerHTML + ',100%,' + this.lightness + '%)';
+            ctx.fill();
+            //ctx.stroke();
+            requestAnimationFrame(this.update.bind(this));
+        }
+    }
+}
+window.addEventListener('mousemove', function(e){
+    const root = new Root(e.x, e.y);
+    root.update();
+})
+window.addEventListener('scroll', function(e){
+    brushcolor.innerHTML++;
+})
+var brushcolor = document.getElementById("brush-color");
+var slider = document.getElementById("brush-color-slider").oninput = function(){
+    brushcolor.innerHTML = this.value
+}
